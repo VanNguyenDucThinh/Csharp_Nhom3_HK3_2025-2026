c5098401a83e3226b5ad13f3c0b3626aa9356d8e
VanNguyenDucThinh <3124410344@sv.sgu.edu.vn>
2026-06-01 23:30:05 +0700

    Test chương trình để tạo database

--- TimHieuDapper/DataAccess/obj/Debug/net10.0/DataAccess.AssemblyInfo.cs
+++ TimHieuDapper/DataAccess/obj/Debug/net10.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7125fcafabebeb41340d56fdc5de7dbaea8325")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c913ca74d0c12b0917bc7ff7f34c1608898749f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
