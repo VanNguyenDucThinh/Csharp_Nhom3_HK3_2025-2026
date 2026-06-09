69d54f32de9ab00eb30cacfb85ceaf650a94e548
Le Truong <lehuytruong844@gmail.com>
2026-06-09 20:06:42 +0700

    update CreateUserProfile(Bool->Guid)

--- BACKEND/TuneVault/TuneVault.Domain/Interfaces/IUserProfileRepository.cs
+++ BACKEND/TuneVault/TuneVault.Domain/Interfaces/IUserProfileRepository.cs
@@ -5,7 +5,7 @@ namespace TuneVault.Domain.Interfaces;
 
 public interface IUserProfileRepository
 {
-    Task<bool> CreateUserProfile(UserProfile userProfile);//Tạo mới người dùng
+    Task<Guid> CreateUserProfile(UserProfile userProfile);//Tạo mới người dùng
     Task<bool> UpdateUserProfile(UserProfile userProfile);//Cập nhật thông tin người dùng
     Task<bool> DeleteUserProfile(Guid userId);//Xóa thông tin người dùng
     Task<UserProfile?> GetUserProfileByIdAsync(Guid userId); //Lấy thông tin người dùng
