d6cc6cae0ea1847648af0a83f7ad4ef32984fc8a
Le Truong <lehuytruong844@gmail.com>
2026-06-09 21:38:48 +0700

    update IPlayHistory

--- BACKEND/TuneVault/TuneVault.Domain/Interfaces/IPlayHistoryRepository.cs
+++ BACKEND/TuneVault/TuneVault.Domain/Interfaces/IPlayHistoryRepository.cs
@@ -5,7 +5,7 @@ namespace TuneVault.Domain.Interfaces;
 
 public interface IPlayHistoryRepository
 {
-    Task<MediaItem> AddMediaItemToPlayHistory (Guid playHistoryId, Guid mediaItemId);//Thêm bài hát vào lịch sử phát
+    Task<bool> AddMediaItemToPlayHistory(PlayHistory playHistoryId);//Thêm bài hát vào lịch sử phát
     Task<bool> DeletePlayHistory(Guid playHistoryId);//Xóa lịch sử phát theo ID
     Task<IEnumerable<PlayHistory>> GetPlayHistoryByUserId(Guid userId);//Lấy lịch sử phát của người dùng theo ID
 
