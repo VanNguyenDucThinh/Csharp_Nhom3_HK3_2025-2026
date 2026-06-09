87690e71275c8de7ce65b6b4cde775c5b0259a07
Le Truong <lehuytruong844@gmail.com>
2026-06-09 22:28:48 +0700

    update CreateDate cho tự lấy thời gian hệ thống

--- BACKEND/TuneVault/TuneVault.Domain/Entities/PlayList.cs
+++ BACKEND/TuneVault/TuneVault.Domain/Entities/PlayList.cs
@@ -8,7 +8,7 @@ public class PlayList
     public Guid Id { get; set; }//khoa chính
     public string? Name { get; set; }//Ten playlist
     public bool IsPublic { get; set; }=true;//Playlist công khai(true) hay riêng tư(false)
-    public DateTime CreatedDate { get; set; }//Ngay tao playlist
-    public Guid Owner { get; set; }//Nguoi tao playlist
+    public DateTime CreatedDate { get; set; }= DateTime.UtcNow;//Ngay tao playlist
+    public Guid Owner { get; set; } //Nguoi tao playlist, khoa ngoai
 
 }
