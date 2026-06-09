d989b140d1b6e3ad37aec08a60c912acdf99c801
Le Truong <lehuytruong844@gmail.com>
2026-06-09 13:10:13 +0700

    add IPasswordHash and update PasswordHasher

---
+++ BACKEND/TuneVault/TuneVault.Domain/Interfaces/IPasswordHash.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuneVault.Domain.Interfaces
+{
+    public interface IPasswordHash
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string password, string passwordHash);
+    }
+}
--- BACKEND/TuneVault/TuneVault.Infrastructure/Security/PasswordHasher.cs
+++ BACKEND/TuneVault/TuneVault.Infrastructure/Security/PasswordHasher.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TuneVault.Domain.Interfaces;
 
 namespace TuneVault.Infrastructure.Security
 {
-    internal class PasswordHasher
+    public class PasswordHasher : IPasswordHash
     {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, passwordHash);
+        }
     }
 }
