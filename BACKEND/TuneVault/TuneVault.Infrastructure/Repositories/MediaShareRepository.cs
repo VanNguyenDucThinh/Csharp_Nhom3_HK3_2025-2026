796892ab921f6a55fbb19eb6fa40d440a633d234
Le Truong <lehuytruong844@gmail.com>
2026-06-09 20:33:48 +0700

    update createMediaShare

--- BACKEND/TuneVault/TuneVault.Infrastructure/Repositories/MediaShareRepository.cs
+++ BACKEND/TuneVault/TuneVault.Infrastructure/Repositories/MediaShareRepository.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TuneVault.Domain.Entities;
+using TuneVault.Domain.Interfaces;
 
 namespace TuneVault.Infrastructure.Repositories
 {
-    internal class MediaShareRepository
+    public class MediaShareRepository : IMediaShareRepository
     {
+        private readonly IDbConnectionGen _connection;
+        public MediaShareRepository(IDbConnectionGen connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<bool> CreateMediaShare(MediaShare mediaShare)
+        {
+            string sql = @"Insert into MediaShare(IdSender , IdReceiver , IdMediaItem, IdPlayList, ShareAt)
+                           values (@IdSender, @IdReceiver, @IdMediaItem, NULL, @ShareAt)";
+            using var connection = _connection.CreateConnection();
+            var command = new CommandDefinition(sql, new
+            {
+                IdSender = mediaShare.IdSender,
+                IdReceiver = mediaShare.IdReceiver,
+                IdMediaItem = mediaShare.IdMediaItem,
+                ShareAt = mediaShare.ShareAt,
+            });
+            int RowsAffected = await connection.ExecuteAsync(command);
+            return RowsAffected> 0;
+        }
     }
 }
