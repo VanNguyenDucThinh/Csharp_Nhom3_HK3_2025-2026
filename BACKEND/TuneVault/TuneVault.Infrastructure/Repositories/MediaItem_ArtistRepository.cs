298ce3f32f8c5ec3739269387f178b1760030c35
Le Truong <lehuytruong844@gmail.com>
2026-06-10 21:50:58 +0700

    update

---
+++ BACKEND/TuneVault/TuneVault.Infrastructure/Repositories/MediaItem_ArtistRepository.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TuneVault.Domain.Entities;
+using TuneVault.Domain.Interfaces;
+
+namespace TuneVault.Infrastructure.Repositories
+{
+    public class MediaItem_ArtistRepository : IMediaItem_ArtistRepository
+    {
+        private readonly IDbConnectionGen _connection;
+        public MediaItem_ArtistRepository(IDbConnectionGen connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<bool> CreateMediaItemArtist(Guid mediaItemId, Guid artistId)
+        {
+            string sql = @"insert into MediaItem_Artist(IdMediaItem, IdArtist)
+                           values(@MediaItemId , @ArtistId)";
+            using var connection = _connection.CreateConnection();
+            var command = new CommandDefinition(sql, new
+            {
+                MediaItemId = mediaItemId,
+                ArtistId = artistId
+            });
+            int RowsAffected = await connection.ExecuteAsync(command);
+            return RowsAffected > 0;
+        }
+
+        public async Task<bool> DeleteMediaItemArtist(Guid mediaItemId, Guid artistId)
+        {
+            string sql = @"delete from MediaItem_Artist
+                           where IdMediaItem = @MediaItemId and IdArtist=@ArtistId";
+            using var connection = _connection.CreateConnection();
+            var command = new CommandDefinition(sql, new
+            {
+                MediaItemId = mediaItemId,
+                ArtistId = artistId
+            });
+            int RowsAffected = await connection.ExecuteAsync(command);
+            return RowsAffected > 0;
+        }
+
+        public async Task<IEnumerable<MediaItem>> GetMediaItemsByArtistId(Guid artistId)
+        {
+            string sql = @"select mi.Id, mi.Title, mi.Duration, mi.UploadDateMediaItem, mi.UrlMediaItem
+                           from MediaItem mi
+                           inner join MediaItem_Artist mia
+                                on mi.Id = mia.IdMediaItem
+                           where mia.IdArtist = @ArtistId
+                           order by mi.UploadDateMediaItem desc";
+            using var connection = _connection.CreateConnection();
+            var command = new CommandDefinition(sql, new
+            {
+                ArtistId = artistId
+            });
+            return await connection.QueryAsync<MediaItem>(command);
+        }
+    }
+}
---
+++ BACKEND/TuneVault/TuneVault.Infrastructure/Repositories/PlayListTrackRepository.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using TuneVault.Domain.Entities;
+using TuneVault.Domain.Interfaces;
+
+namespace TuneVault.Infrastructure.Repositories
+{
+    public class PlayListTrackRepository : IPlayListTrackRepository
+    {
+        private readonly IDbConnectionGen _connection;
+
+        public PlayListTrackRepository(IDbConnectionGen connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<bool> AddTrackToPlaylist(Guid playlistId, Guid mediaItemId)
+        {
+            string sql = @"insert into PlayListTrack(IdPlaylist , IdMediaItem)
+                           values (@PlayListId , @MediaItemId)";
+            using var connection = _connection.CreateConnection();
+            var command = new CommandDefinition(sql, new
+            {
+                PlayListId = playlistId,
+                MediaItemId = mediaItemId,
+            });
+            int RowsAffected = await connection.ExecuteAsync(command);
+            return RowsAffected > 0;
+        }
+
+        public async Task<IEnumerable<MediaItem>> GetTracksInPlaylist(Guid playlistId)
+        {
+            string sql = @"select mi.Id, mi.Title, mi.Duration, mi.UrlMediaItem 
+                           from MediaItem mi
+                           inner join PlayListTrack plt
+                                 on mi.Id = plt.IdMediaItem
+                           where plt.IdPlaylist = @PlayListId";
+            //Neu can lay them ten tac gia thi can them 2 bang nua
+            //inner join MediaItem_Artist mia
+            //on mi.Id = mia.IdMediaItem
+            //inner join Artist art
+            //on mia.IdArtist = art.Id
+            using var connection = _connection.CreateConnection();
+
+            var command = new CommandDefinition(sql, new
+            {
+                PlaylistId = playlistId,
+            });
+            return await connection.QueryAsync<MediaItem>(command);
+        }
+
+        public async Task<bool> RemoveTrackFromPlaylist(Guid playlistId, Guid mediaItemId)
+        {
+            string sql = @"delete from PlayListTrack
+                           where IdPlaylist = @PlayListId and MediaItemId = @MediaItemId";
+            using var connection = _connection.CreateConnection();
+            var command = new CommandDefinition(sql, new
+            {
+                PlayListId = playlistId,
+                MediaItemId = mediaItemId,
+            });
+            int RowsAffected = await connection.ExecuteAsync(command);
+            return RowsAffected > 0;
+        }
+    }
+}
