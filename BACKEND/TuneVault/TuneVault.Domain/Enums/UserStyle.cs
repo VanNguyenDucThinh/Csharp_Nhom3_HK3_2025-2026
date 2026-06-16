d49bee6d9d6370ff3fc46450b9fbb129e98ae5e9
nvthuan06 <nguyenvuthuan2k6@gmail.com>
2026-06-17 02:32:38 +0700

    Add files via upload

---
+++ BACKEND/TuneVault/TuneVault.Domain/Enums/UserStyle.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TuneVault.Domain.Enums;
+
+public enum UserStyle
+{
+    User =0,
+    Artist=1
+
+}
--- BACKEND/TuneVault/TuneVault.Domain/Interfaces/IPlayListRepository.cs
+++ BACKEND/TuneVault/TuneVault.Domain/Interfaces/IPlayListRepository.cs
@@ -9,5 +9,6 @@ public interface IPlayListRepository
     Task<bool> UpdatePlayList(PlayList playList);//Cập nhật thông tin danh sách phát
     Task<bool> DeletePlayList(Guid playListId);//Xóa danh sách phát theo ID
     Task<PlayList> GetPlayListById(Guid playListId);//Lấy thông tin playlist
+    Task<List<PlayList>> GetPlayListByTitle(string title, int skip, int take);
 
 }
