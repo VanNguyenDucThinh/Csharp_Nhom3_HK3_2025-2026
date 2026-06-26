0b9690444f4b2d35e21d9aa8113f27c35ea6ff7b
HuynhThaiKiet <htk2k6@gmail.com>
2026-06-26 17:48:15 +0700

    delete redundant folder

--- BACKEND/TuneVault/TuneVault.Application/Interface/ICurrentUserService.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace TuneVault.Application.Interface;
-
-public interface ICurrentUserService//ở trên tầng API có sử dụng HttpContext gửi xuống đây 
-{
-    string? UserId {get; }
-
-}
--- BACKEND/TuneVault/TuneVault.Domain/Enums/MediaStyle.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace TuneVault.Domain.Enums;
-
-public enum MediaStyle
-{
-    Audio = 0,
-    Video = 1,
-}
