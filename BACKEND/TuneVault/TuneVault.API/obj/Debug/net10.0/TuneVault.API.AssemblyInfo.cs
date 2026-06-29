48ea82deea6c84278a62318087a64d54aea46939
HuynhThaiKiet <htk2k6@gmail.com>
2026-06-29 20:29:20 +0700

    cập nhật nhảm nhí

--- BACKEND/TuneVault/TuneVault.API/obj/Debug/net10.0/TuneVault.API.AssemblyInfo.cs
+++ BACKEND/TuneVault/TuneVault.API/obj/Debug/net10.0/TuneVault.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuneVault.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea1409cc65485979ae0d4624ea1e731584f78218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2af3af9a13b3b007ca5e1ddc6ad91db652d90cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuneVault.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuneVault.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
